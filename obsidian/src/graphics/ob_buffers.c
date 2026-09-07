26b4b4732fbf2f0eaf9117ab7e8b459e17a498bc
jaythom2723 <penguinking684@gmail.com>
2026-09-07 11:02:27 -0500

    ebos but broken

--- obsidian/src/graphics/ob_buffers.c
+++ obsidian/src/graphics/ob_buffers.c
@@ -11,6 +11,7 @@
 
 #define __OB_MAX_VBOS 0xFF
 #define __OB_MAX_EBOS 0xFF
+#define __OB_MAX_VAOS 0xFF
 
 bool __ob_buf_initmodule(void);
 bool __ob_buf_closemodule(void);
@@ -22,6 +23,10 @@ bool __ob_buf_setvbodata(uint32_t, size_t, const void*, uint32_t);
 void __ob_buf_unbindvbo(void);
 
 uint32_t __ob_buf_createebo(void); // TODO: RnD
+bool __ob_buf_deleteebo(uint32_t);
+bool __ob_buf_bindebo(uint32_t);
+bool __ob_buf_setebodata(uint32_t, size_t, const void*, uint32_t);
+void __ob_buf_unbindebo(void);
 
 extern bool __ob_error_pusherror(enum obsidian_error_code, enum obsidian_error_severity, enum obsidian_error_category, const char*, const char*, const uint32_t);
 extern bool __ob_error_readerror(void);
@@ -29,21 +34,24 @@ extern bool __ob_error_readerror(void);
 extern void __ob_log_wline(enum ob_logger_message_type, const char* const);
 extern void __ob_log_wsline(const char* const);
 
+static uint32_t* vaos = NULL;
 static uint32_t* vbos = NULL;
 static uint32_t* ebos = NULL;
 
+static uint32_t* vaoptr = NULL;
 static uint32_t* vboptr = NULL;
 static uint32_t* eboptr = NULL;
 
 bool __ob_buf_initmodule(void)
 {
-    if (vbos != NULL || ebos != NULL)
+    if (vbos != NULL || ebos != NULL || vaos != NULL)
         return false;
 
     vbos = calloc(__OB_MAX_VBOS, sizeof(uint32_t));
     ebos = calloc(__OB_MAX_EBOS, sizeof(uint32_t));
+    vaos = calloc(__OB_MAX_VAOS, sizeof(uint32_t));
 
-    if (vbos == NULL || ebos == NULL)
+    if (vbos == NULL || ebos == NULL || vaos == NULL)
     {
         (void)__ob_error_pusherror(ERR_MODULE_INIT, SEV_FATAL, CAT_CORE_SYS, "Failed to initialize Obisidian: Buffers Core Module", __FILE__, __LINE__);
         (void)__ob_error_readerror();
@@ -52,7 +60,9 @@ bool __ob_buf_initmodule(void)
 
     memset(vbos, 0, __OB_MAX_VBOS * sizeof(uint32_t));
     memset(ebos, 0, __OB_MAX_EBOS * sizeof(uint32_t));
+    memset(vaos, 0, __OB_MAX_VAOS * sizeof(uint32_t));
 
+    vaoptr = vaos;
     vboptr = vbos;
     eboptr = ebos;
 
@@ -61,7 +71,7 @@ bool __ob_buf_initmodule(void)
 
 bool __ob_buf_closemodule(void)
 {
-    if (vbos == NULL || ebos == NULL)
+    if (vbos == NULL || ebos == NULL || vaos == NULL)
         return false;
 
     // delete any buffers that haven't yet been deleted
@@ -79,8 +89,22 @@ bool __ob_buf_closemodule(void)
         glDeleteBuffers(1, (ebos + i));
     }
 
+    // do the same for vertex arrays
+    for (int i = 0; i < __OB_MAX_VAOS; i++)
+    {
+        if (glIsVertexArray(*(vaos + i)) == GL_FALSE)
+            continue;
+        glDeleteVertexArrays(1, (vaos + i));
+    }
+
+    vaoptr = NULL;
+    vboptr = NULL;
+    eboptr = NULL;
+
+    free(vaos);
     free(vbos);
     free(ebos);
+    vaos = NULL;
     vbos = NULL;
     ebos = NULL;
 
@@ -112,7 +136,11 @@ bool __ob_buf_deletevbo(uint32_t i)
 {
     uint32_t* buffer = (vbos + i);
     if (glIsBuffer(*buffer) == GL_FALSE)
+    {
+        // TODO: replace with log/error
+        printf("Failed to delete VBO!\n");
         return false;
+    }
     glDeleteBuffers(1, buffer);
     return true;
 }
@@ -149,4 +177,73 @@ bool __ob_buf_setvbodata(uint32_t i, size_t sbytes, const void* data, uint32_t u
 void __ob_buf_unbindvbo(void)
 {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+/*
+uint32_t __ob_buf_createebo(void); // TODO: RnD
+bool __ob_buf_deleteebo(uint32_t);
+bool __ob_buf_bindebo(uint32_t);
+bool __ob_buf_setebodata(uint32_t, size_t, const void*, uint32_t);
+void __ob_buf_unbindebo(void);
+*/
+
+uint32_t __ob_buf_createebo(void)
+{
+    if (ebos == NULL || vbos == NULL || vaos == NULL)
+        return 0xFF;
+
+    if((eboptr - ebos) >= __OB_MAX_EBOS)
+        return 0xFF;
+
+    uint32_t index = (uint32_t)(eboptr - ebos);
+    glGenBuffers(1, eboptr);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *eboptr);
+    eboptr++;
+    return index;
+}
+
+bool __ob_buf_deleteebo(uint32_t index)
+{
+    if (ebos == NULL || vbos == NULL || vaos == NULL)
+        return false;
+
+    uint32_t* buffer = (ebos + index);
+    if (glIsBuffer(*buffer) == GL_FALSE)
+        return false;
+
+    glDeleteBuffers(1, ebos + index);
+    return true;
+}
+
+bool __ob_buf_bindebo(uint32_t index)
+{
+    if (ebos == NULL || vbos == NULL || vaos == NULL)
+        return false;
+
+    uint32_t* buffer = (ebos + index);
+    if (glIsBuffer(*buffer) == GL_FALSE)
+        return false;
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *buffer);
+    return true;
+}
+
+bool __ob_buf_setebodata(uint32_t index, size_t sbytes, const void* data, uint32_t usage)
+{
+    if (ebos == NULL || vbos == NULL || vaos == NULL)
+        return false;
+
+    uint32_t* buffer = (ebos + index);
+    if (glIsBuffer(*buffer) == GL_FALSE)
+        return false;
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *buffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sbytes, data, usage);
+
+    return true;
+}
+
+void __ob_buf_unbindebo(void)
+{
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
 }
--- space-game/src/main.c
+++ space-game/src/main.c
@@ -2,19 +2,38 @@
 #include <display/ob_window.h>
 #include <graphics/ob_shader.h>
 
+// float vertices[] = {
+//     0.0f, 1.0f,     0.0f, 1.0f,
+//     1.0f, 0.0f,     1.0f, 0.0f,
+//     0.0f, 0.0f,     0.0f, 0.0f,
+//     0.0f, 1.0f,     0.0f, 1.0f,
+//     1.0f, 1.0f,     1.0f, 1.0f,
+//     1.0f, 0.0f,     1.0f, 0.0f    
+// };
+
 float vertices[] = {
-    0.0f, 1.0f,     0.0f, 1.0f,
-    1.0f, 0.0f,     1.0f, 0.0f,
-    0.0f, 0.0f,     0.0f, 0.0f,
-    0.0f, 1.0f,     0.0f, 1.0f,
-    1.0f, 1.0f,     1.0f, 1.0f,
-    1.0f, 0.0f,     1.0f, 0.0f    
+    0.5f, 0.5f,     1.0f, 1.0f,     // top right
+    0.5f, -0.5f,    1.0f, -1.0f,     // bottom right
+    -0.5f, 0.5f,    -1.0f, 1.0f,     // top left
+    0.5f, -0.5f,    1.0f, -1.0f,     // bottom right
+    -0.5f, -0.5f,   -1.0f, -1.0f,     // bottom left
+    -0.5f, 0.5f,    -1.0f, 1.0f,     // top left
+};
+
+unsigned int indices[] = {
+    0, 1, 3,
+    1, 2, 3
 };
 
 extern uint32_t __ob_buf_createvbo(void);
 extern bool __ob_buf_deletevbo(uint32_t);
 extern bool __ob_buf_bindvbo(uint32_t);
 extern bool __ob_buf_setvbodata(uint32_t, size_t, const void*, uint32_t);
+
+extern uint32_t __ob_buf_createebo(void);
+extern bool __ob_buf_deleteebo(uint32_t);
+extern bool __ob_buf_bindebo(uint32_t);
+extern bool __ob_buf_setebodata(uint32_t, size_t, const void*, uint32_t);
 
 int main(void)
 {
@@ -39,22 +58,21 @@ int main(void)
 
     // TODO: internalize
     // TODO: perhaps make an exposed asset loading system that can create asset prototypes via coordinate files (3D models?)
-    uint32_t vao, vbo;
+    uint32_t vao, vbo, ebo;
 
     glGenVertexArrays(1, &vao);
     vbo = __ob_buf_createvbo();
-    printf("%d\n", vbo);
+    ebo = __ob_buf_createebo();
 
     glBindVertexArray(vao);
 
     (void)__ob_buf_setvbodata(vbo, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    (void)__ob_buf_setebodata(ebo, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2*sizeof(float)));
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     while (OBWNDshouldClose() == false)
     {
@@ -62,7 +80,8 @@ int main(void)
 
         OBSHDRuseProgram(program);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        __ob_buf_bindebo(ebo);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         OBWNDswapBuffers();
     }
