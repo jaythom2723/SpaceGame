5b83e883a83b4f8765ff0853c269cec7b15454df
jaythom2723 <penguinking684@gmail.com>
2026-09-08 12:10:47 -0500

    more clarg work

--- obtexcvtr/src/main.c
+++ obtexcvtr/src/main.c
@@ -1,134 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <string.h>
 
-/*
-    -x delete origin image
-    -v verbose mode
-    -l set log output path
-    -f input file format
-    -q quiet mode
-*/
+#include <stb-image/stb_image.h>
 
-enum cli_flags {
-    DELETE_ORIGIN_IMAGE = 0b000001,
-    VERBOSE_MODE        = 0b000010,
-    LOG_OUTPUT_PATH     = 0b000100,
-    INPUT_FILE_FORMAT   = 0b001000,
-    QUIET_MODE          = 0b010000,
-    OUTPUT_PATH         = 0b100000,
-};
+#define DEPTH_RGBA                  32
+#define DEPTH_RGB                   24
+#define DEPTH_RGBA16                16
+#define DEPTH_RGB16                 16
 
-enum input_format {
-    FORMAT_PNG,
-    FORMAT_JPG,
-    FORMAT_BMP,
-};
+#define DEFAULT_FILE_EXT            ".obtf"
+#define DEFAULT_COLOR_BIT_DEPTH     DEPTH_RGBA;
+#define DEFAULT_OUTPUT_PATH         "./"
 
-// TODO: develop a help page/man page for this tool
+#define FLAG_SET_OUTPUT             "-o"
+#define FLAG_SHOW_HELP              "-h"
+#define FLAG_SET_DEPTH              "-d"
 
-bool __ob_check_cli_argument(const uint32_t, uint32_t*, char**);
-
-static enum input_format iformat = -1;
-static bool verbose_mode = false;
-static bool quiet_mode = false;
-static bool delete_origin_image = false;
+// static bool display_help = false;
 static char* input_path = NULL;
 static char* output_path = NULL;
-static char* log_output_path = NULL;
+// static uint32_t bit_depth = DEFAULT_COLOR_BIT_DEPTH;
+
+void __ob_clone_string(char** restrict dest, const char* restrict src);
+void __ob_process_clargs(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
-    if (argc <= 1)
-        return -1; // TODO: print usage/help page
+    argv++;
+    argc--;
 
-    uint32_t index = 0;
-    uint32_t offset = 1;
-    while (argc != 0)
-    {
-        offset = 1;
-        if (__ob_check_cli_argument(index, &offset, argv))
-        {
+    __ob_process_clargs(argc, argv);
 
-        }
-        index += offset;
-        argc--;
-    }
+    printf("%s\n%s\n", input_path, output_path);
 
-    /*
-    static char* input_path = NULL;
-static char* output_path = NULL;
-static char* log_output_path = NULL;
-    */
-
-    free(input_path);
     free(output_path);
-    if (log_output_path != NULL)
-        free(log_output_path);
+    free(input_path);
 
     return 0;
 }
 
-bool __ob_check_cli_argument(const uint32_t index, uint32_t* offset, char** argv)
+void __ob_clone_string(char** restrict dest, const char* restrict src)
 {
-    char* arg = *(argv + index);
-    printf("%s\n", arg);
+    (*dest) = calloc(strlen(src)+1, sizeof(char));
+    if (dest == NULL)
+        return;
+    strcpy(*dest, src);
+}
 
-    if (strcmp(arg, "-o") == 0)
+// TODO: come up with a better way to do this bullshit
+void __ob_process_clargs(int argc, char** argv)
+{
+    for (int i = 0; i < argc; i++)
     {
-        char* path = *(argv + (index + 1));
-        (*offset) ++;
-        output_path = calloc(strlen(path)+1, sizeof(char));
-        if(output_path == NULL)
-            return false;
-        strcpy(output_path, path);
-        return true;
-    }
+        char* arg = *(argv + i);
 
-    if (strcmp(arg, "-x") == 0)
-    {
-        delete_origin_image = true;
-        return true;
+        if (strcmp(arg, FLAG_SET_OUTPUT) == 0)
+        {
+            char* op = *(argv + (++i));
+            __ob_clone_string(&output_path, op);
+        } else
+        {
+            // This should be the input file, if its not, someone fucked up lol
+            __ob_clone_string(&input_path, arg);
+        }
     }
-
-    if (strcmp(arg, "-v") == 0 && quiet_mode == false)
-    {
-        verbose_mode = true;
-        return true;
-    }
-
-    if (strcmp(arg, "-l") == 0)
-    {
-        char* path = *(argv + (index + 1));
-        (*offset) ++;
-        log_output_path = calloc(strlen(path)+1, sizeof(char));
-        if (log_output_path == NULL)
-            return false;
-        strcpy(log_output_path, path);
-        return true;
-    }
-
-    if (strcmp(arg, "-f") == 0)
-    {
-        char* format = *(argv + (index + 1));
-        // TODO: segfault here
-        (*offset) ++;
-        if (strcmp(format, "PNG") == 0 || strcmp(format, "png") == 0)
-            iformat = FORMAT_PNG;
-        if (strcmp(format, "JPG") == 0 || strcmp(format, "jpg") == 0)
-            iformat = FORMAT_JPG;
-        if (strcmp(format, "BMP") == 0 || strcmp(format, "bmp") == 0)
-            iformat = FORMAT_BMP;
-        return true;
-    }
-
-    if (strcmp(arg, "-q") == 0 && verbose_mode == false)
-    {
-        quiet_mode = true;
-        return true;
-    }
-
-    return false;
 }
